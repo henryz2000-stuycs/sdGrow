b78b203a02c84bf9cfa5dbdf01cf88a5a95c55ed
henryz2000 <hzheng3@stuy.edu>
2018-02-07 20:57:17 -0500

    modulo

--- canvas.js
+++ canvas.js
@@ -4,7 +4,7 @@ var canvas = document.getElementById("slate");
 var canvasContext = canvas.getContext('2d');
 canvasContext.fillStyle = "#FF0000";
 
-var requestID;
+var requestID, end;
 
 var clearCallback = function(e){
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
@@ -20,16 +20,19 @@ var canvasCallback = function(e){
     var run = function(){
 	clearCallback();
 	canvasContext.beginPath();
-	if (((canvas.width+canvas.height/100)+requestID) == canvas.width+canvas.height/2){
+	if (requestID == (canvas.width+canvas.height)/4){
+	    end = requestID;
+	}
+	if (requestID >= (canvas.width+canvas.height)/4){
 	    console.log(((canvas.width+canvas.height)/100)-(1-requestID));
-	    canvasContext.arc(canvas.width/2, canvas.height/2, ((canvas.width+canvas.height)/100)-(1-requestID), 0, 2*Math.PI);
+	    canvasContext.arc(canvas.width/2, canvas.height/2, requestID-(requestID % end), 0, 2*Math.PI);
 	}
 	else{
-	    canvasContext.arc(canvas.width/2, canvas.height/2, ((canvas.width+canvas.height)/100)+requestID, 0, 2*Math.PI);
+	    canvasContext.arc(canvas.width/2, canvas.height/2, requestID, 0, 2*Math.PI);
 	}
 	canvasContext.fill();
 	requestID = window.requestAnimationFrame(run);
-	console.log("ran " + requestID);
+	//console.log("ran " + requestID);
     };
     window.requestAnimationFrame(run);
 };
